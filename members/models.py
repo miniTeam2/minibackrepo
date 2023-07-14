62cc4b049ce6776c9e390f5e6918e6bf85334752
Leespect <wewqwew153@gmail.com>
2023-07-14 16:37:29 +0900

    sdfad

--- members/models.py
+++ members/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomUser(models.Model):
-    nickname = models.CharField(max_length=100)
-    username = models.CharField(max_length=100)
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
--- members/serializers.py
+++ members/serializers.py
@@ -4,9 +4,18 @@ from .models import CustomUser
 class LoginSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=100)
     password = serializers.CharField(max_length=100)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'password')
 
 
 class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=100)
+    password = serializers.CharField(max_length=100)
+    nickname = serializers.CharField(max_length=100)
+    email = serializers.CharField(max_length=100)
+
     class Meta:
         model = CustomUser
-        fields = ['nickname', 'username', 'password', 'email']
+        fields = ('username', 'password', 'nickname', 'email')
