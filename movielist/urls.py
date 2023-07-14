ba349a2175b7617187df07fe328d45287565fc16
Leespect <wewqwew153@gmail.com>
2023-07-14 15:56:58 +0900

    movie detail

--- movielist/urls.py
+++ movielist/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from . import views_detail
 from rest_framework import routers
 
 app_name = 'movielist'
@@ -7,6 +8,7 @@ app_name = 'movielist'
 urlpatterns = [
     path('', views.init_db),
     path('movies/', views.MovieList.as_view()),
+    path('detail/<str:title_kor>/', views_detail.MovieDetail.as_view()),
     # path('movies/', views.MovieList.as_view({'get': 'list'})),
     # path('init_db/', views.init_db)
 ]
