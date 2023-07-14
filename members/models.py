ccd25387887e85058fdd32a809a1c370b113213a
ShinSub98 <wjdtjq26@naver.com>
2023-07-14 19:43:15 +0900

    이메일 주석처리

--- members/models.py
+++ members/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class CustomUser(AbstractUser):
     REQUIRED_FIELDS = []
-    email = None
+    # email = None
     nickname = models.CharField(max_length=100, unique=True)
     username = models.CharField(max_length=100, unique=True)
     password = models.CharField(max_length=100)
